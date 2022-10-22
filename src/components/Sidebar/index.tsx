c267b75eb39c5cd73a1699e0bc01cd4b8fe5c97b
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-10-22 16:38:33 -0300

    Commit inicial

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+import { Container } from './styles';
+
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg';
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg';
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg';
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg';
+
+import menuImg from '../../assets/menu.svg';
+
+export function Sidebar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <Container isMenuOpen={menuOpen}>
+      <button type='button' onClick={handleToggleMenu}>
+        <img src={menuImg}/>
+      </button>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#" className='active'>
+              <BurgerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+          
+          <li>
+            <a href="#">
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <SodaPopIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <IceCreamIcon />
+              <span>Sorvetes</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
+}
