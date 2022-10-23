ad66fe39990ad9b9ebf0dcfe5118ad97060915c3
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-10-23 19:08:21 -0300

    FEAT: Criando componente Snack

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,11 +1,32 @@
 import { Head } from '../../../components/Head';
+import { Snacks } from '../../../components/Snacks';
 import { SnackTitle } from '../../../components/SnackTitle';
 
 export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burger',
+      name: 'Mega',
+      description: 'O artesanal tamanho família recheado com três carnes suculentas, queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/upjIUnG.jpg'
+    },
+    {
+      id: 2,
+      snack: 'burger',
+      name: 'Extra bacon',
+      description: 'Criado para os amantes do bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/B4J04AJ.jpg'
+    }
+  ];
+
   return (
     <>
       <Head title="Hambúrgueres" description="Nossos melhores burgers" />
       <SnackTitle>Hambúrgueres</SnackTitle>
+      <Snacks snacks={data}></Snacks>
     </>
   );
 }
